e836090cdf0b91548e366187a69f1e5b05fcbc89
SELVAMANI-SM <selva872000@gmail.com>
2022-05-06 08:46:25 +0530

    first commit

---
+++ src/test/java/com/librarybooksystem/LibraybooksystemRegisteryMsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.librarybooksystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraybooksystemRegisteryMsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
